8c75942610ae6a1738c750d270da6d42d16cda7d
Vili Vugrinec <vvugrinec21@student.foi.hr>
2023-05-18 19:23:37 +0200

    Izrada baze podataka

--- Software/WalletWatch/WalletWatch/Form1.Designer.cs
+++ Software/WalletWatch/WalletWatch/Form1.Designer.cs
@@ -15,6 +15,7 @@
         {
             if (disposing && (components != null))
             {
+
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -28,10 +29,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(719, 400);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
